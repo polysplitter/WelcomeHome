3742efa599151cc3d85df6c5e7381be1532f1920
Kyle <polysplitter@gmail.com>
2019-09-08 01:36:35 -0500

    more updates

--- Core.lua
+++ Core.lua
@@ -1,4 +1,6 @@
-message = "Banana"
+message = "Sir! Home Base ahead!"
+showInChat = false
+showOnScreen = true
 
 local options = {
     type = 'group',
@@ -13,6 +15,28 @@ local options = {
             end,
             set = function(value)
                 message = value
+            end,
+        },
+        showInChat = {
+            type = 'toggle',
+            name = 'Show in Chat',
+            desc = 'Toggles the display of the message in the chat window',
+            get = function()
+                return showInChat
+            end,
+            set = function()
+                showInChat = not showInChat
+            end,
+        },
+        showOnScreen = {
+            type = 'toggle',
+            name = 'Show on Screen',
+            desc = 'Toggles the display of the message on the screen',
+            get = function()
+                return showOnScreen
+            end,
+            set = function()
+                showOnScreen = not showOnScreen
             end,
         },
     },
@@ -33,18 +57,37 @@ function WelcomeHome:OnEnable()
 end
 
 function WelcomeHome:ZONE_CHANGED()
-    self:Print("You have changed zones! " .. UnitName('player') .. " " .. GetRealZoneText())
-    self:Print("Your bind location is: " .. GetBindLocation())
-    if GetBindLocation() == GetSubZoneText() then
-        self:Print(message)
+    if showInChat then
+        self:Print("You have changed zones! " .. UnitName('player') .. ", You arrived at: " .. GetSubZoneText())
+        self:Print("Your bind location is: " .. GetBindLocation())
+        if GetBindLocation() == GetSubZoneText() then
+            self:Print(message)
+        end
+    end
+
+    if showOnScreen then 
+        UIErrorsFrame:AddMessage("You have changed zones! " .. UnitName('player') .. ", You arrived at: " .. GetSubZoneText())
+        UIErrorsFrame:AddMessage("Your bind location is: " .. GetBindLocation())
+        if GetBindLocation() == GetSubZoneText() then
+            UIErrorsFrame:AddMessage(message)
+        end
     end
 end
 
 function WelcomeHome:ZONE_CHANGED_NEW_AREA()
-    self:Print("You have changed zones! " .. UnitName('player') .. " " .. GetSubZoneText())
-    self:Print("Your bind location is: " .. GetBindLocation())
-    if GetBindLocation() == GetRealZoneText() then
-        self:Print(message)
+    if showInChat then
+        self:Print("You have changed zones! " .. UnitName('player') .. ", You arrived at: " .. GetRealZoneText())
+        self:Print("Your bind location is: " .. GetBindLocation())
+        if GetBindLocation() == GetRealZoneText() then
+            self:Print(message)
+        end
+    end
+    if showOnScreen then
+        UIErrorsFrame:AddMessage("You have changed zones! " .. UnitName('player') .. ", You arrived at: " .. GetRealZoneText())
+        UIErrorsFrame:AddMessage("Your bind location is: " .. GetBindLocation())
+        if GetBindLocation() == GetRealZoneText() then
+            UIErrorsFrame:AddMessage(message)
+        end
     end
 end
  
