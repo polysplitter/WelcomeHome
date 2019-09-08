c11aed868266e860579380ac449fa1e8fb1fca8e
Kyle <polysplitter@gmail.com>
2019-09-07 22:54:03 -0500

    just set message as a local var

--- Core.lua
+++ Core.lua
@@ -1,4 +1,23 @@
-WelcomeHome = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0")
+message = "Banana"
+
+local options = {
+    type = 'group',
+    args = {
+        msg = {
+            type = 'text',
+            name = 'msg',
+            desc = 'The message text to be displayed',
+            usage = '<Your message here>',
+            get = 'GetMessage',
+            set = function(value)
+                message = value
+            end,
+        },
+    },
+}
+
+WelcomeHome = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceEvent-2.0")
+WelcomeHome:RegisterChatCommand({"/welcomehome", "/wh"}, options)
 
 function WelcomeHome:OnInitialize()
     -- Called when the addon is loaded
@@ -7,7 +26,25 @@ end
 function WelcomeHome:OnEnable()
     -- Called when the addon is enabled
     self:Print("Enabled: Hello World!")
+    self:Print("zones " .. GetRealZoneText() .. " " .. GetSubZoneText())
+    self:RegisterEvent("ZONE_CHANGED")
 end
+
+function WelcomeHome:ZONE_CHANGED()
+    self:Print("You have changed zones! " .. UnitName('player') .. " " .. GetRealZoneText())
+    self:Print("Your bind location is: " .. GetBindLocation())
+    if GetBindLocation() == GetSubZoneText() then
+        self:Print(message)
+    end
+end
+
+function WelcomeHome:GetMessage() 
+    return message
+end
+
+-- function WelcomeHome.SetMessage(newValue)
+--     WelcomeHome.message = newValue
+-- end
 
 function WelcomeHome:OnDisable()
     -- Called when the addon is disabled
